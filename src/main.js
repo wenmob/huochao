28bd02d8ff9180648451f248fa791a860fcadf8a
Chengxw <xiaoke521wen@qq.com>
2018-10-18 22:56:40 +0800

    configzero 2018/10/18 22:56

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 /**
  * 引入vant
  */
 import Vant from 'vant'
 import 'vant/lib/vant-css/index.css'
+
+require('es6-promise').polyfill() // 解决老安卓系统兼容问题
 Vue.use(Vant)
 
 Vue.config.productionTip = false
