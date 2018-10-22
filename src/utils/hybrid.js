00bcd548797ee69123b8147612c7090dedb16b58
zhuxianbin <zhuxian123@qq.com>
2018-10-22 12:04:56 +0800

    增加了hybrid

---
+++ src/utils/hybrid.js
@@ -0,0 +1,20 @@
+import { browser, Cookies } from "./index";
+// import Cookies from "js-cookie";
+// const NativeKey = "xlg-native-object";
+
+export function sendMessage(action, data = {}, callback) {
+  if (callback) {
+    window[`fn_${action}`] = callback;
+  }
+  if (browser.ios)
+    return window.webkit.messageHandlers[action].postMessage(data);
+  if (browser.android) {
+    return window.tb_android[action](JSON.stringify(data));
+  }
+}
+
+// export function getNativeObject() {
+//   const ua = window.navigator.userAgent;
+//   const [x, jsonStr] = ua.split("&&");
+//   return jsonStr ? JSON.parse(jsonStr) : {};
+// }
---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+import Cookies from "js-cookie";
+export const browser = (function() {
+  var u = window.navigator.userAgent;
+  var ios = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+  var wechat = !!u.match(/MicroMessenger/);
+  var android = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1;
+  var native = !!u.match(/&&/);
+  var flag = wechat ? "wechat" : native ? "native" : ios ? "ios" : "android";
+  return {
+    userAgent: u,
+    ios,
+    wechat,
+    android,
+    native,
+    flag
+  };
+})();
+
+export { Cookies };
